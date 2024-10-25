08313ceed1015025092ebce5bd582bdee72268ec
Shubhankar Sawant <shubhankar@Shubhankars-MacBook-Air.local>
2024-10-25 17:01:03 +0530

    Add GitHub Action for updating NIFTY data

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NiftyHeatmap from './components/NiftyHeatmap'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <NiftyHeatmap />
+    </div>
+  )
+}
+
+export default App
